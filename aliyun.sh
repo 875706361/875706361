a7e378f5ba2a3b3c2f9ab4b1b6dc3b2d52a451b1
875706361 <875706361@qq.com>
2020-02-15 16:47:28 +0800

    Add files via upload

---
+++ aliyun.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+wget http://update.aegis.aliyun.com/download/uninstall.sh
+chmod +x uninstall.sh
+./uninstall.sh
+wget http://update.aegis.aliyun.com/download/quartz_uninstall.sh
+chmod +x quartz_uninstall.sh
+./quartz_uninstall.sh
+pkill aliyun-service
+rm -fr /etc/init.d/agentwatch /usr/sbin/aliyun-service
+rm -rf /usr/local/aegis*
+echo "脚本执行完毕，安骑士防护软件已卸载"
