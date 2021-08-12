f498530598823df06380ce11581110c721e59919
875706361 <875706361@qq.com>
2021-08-12 21:39:16 +0800

    Update v2-ui_install.sh

--- v2-ui_install.sh
+++ v2-ui_install.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-cd
-wget https://github.com/875706361/875706361/raw/master/v2-ui-linux.tar.gz
+cd /root/
+wget https://od.lk/d/MThfMjA3Mzc3NDFf/v2-ui-linux.tar.gz
 mv v2-ui-linux.tar.gz /usr/local/
 cd /usr/local/
 tar zxvf v2-ui-linux.tar.gz
 rm v2-ui-linux.tar.gz -f
 cd v2-ui
-chmod +x v2-ui bin/v2ray-v2-ui bin/v2ctl
+chmod +x v2-ui bin/xray-v2-ui
 cp -f v2-ui.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable v2-ui
 systemctl restart v2-ui
-cd /tmp
-wget https://raw.githubusercontent.com/sprov065/v2-ui/master/v2-ui.sh
-mv v2-ui.sh /usr/bin/v2-ui
-chmod 777 /usr/bin/v2-ui
-cd
-bash <(curl -s -L https://233blog.com/v2ray.sh)
+curl -o /usr/bin/v2-ui -Ls https://raw.githubusercontent.com/875706361/875706361/master/v2-ui.sh
+chmod +x /usr/bin/v2-ui
 v2-ui
