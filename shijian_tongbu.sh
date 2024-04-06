e02526fef1fc2ce9cd27f9029ba3a704e0d7221a
875706361 <875706361@qq.com>
2024-04-06 21:47:29 +0800

    Update shijian_tongbu.sh

--- shijian_tongbu.sh
+++ shijian_tongbu.sh
@@ -30,12 +30,21 @@ ntpdate cn.pool.ntp.org
 
 # 将同步后的时间加入系统时间中
 hwclock -w
+
+# 输出提示信息
+echo "时间已同步"
 EOF
 
 # 添加执行权限
 chmod +x /home/shijian.sh
 
-# 将脚本添加到 cron 任务中，每天1:30自动执行
-(crontab -l 2>/dev/null; echo "30 1 * * * /home/shijian.sh") | crontab -
+# 检查是否存在定时任务，如果不存在则添加到 cron 任务中
+if ! crontab -l | grep -q '/home/shijian.sh'; then
+    # 将脚本添加到 cron 任务中，每天1:30自动执行
+    (crontab -l 2>/dev/null; echo "30 1 * * * /home/shijian.sh") | crontab -
+    echo "定时任务已创建"
+else
+    echo "定时任务已存在，无需创建"
+fi
 
 echo "脚本已创建：/home/shijian.sh，并添加到每天1:30自动运行任务中"
