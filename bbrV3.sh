d4a2fa8d6c82d5f51af1f6ac6af6a007f32e22c0
875706361 <875706361@qq.com>
2024-12-04 21:47:31 +0800

    Update bbrV3.sh

--- bbrV3.sh
+++ bbrV3.sh
@@ -18,22 +18,64 @@ CENTOS_IMAGE_URL="https://github.com/Zxilly/bbr-v3-pkg/releases/download/2024-03
 # Function to install BBR v3 on Ubuntu/Debian
 install_bbrv3_ubuntu() {
     wget "$UBUNTU_HEADERS_URL" -O linux-headers.deb
+    if [ $? -ne 0 ]; then
+        echo "下载 linux-headers.deb 失败!"
+        exit 1
+    fi
+
     wget "$UBUNTU_IMAGE_URL" -O linux-image.deb
+    if [ $? -ne 0 ]; then
+        echo "下载 linux-image.deb 失败!"
+        exit 1
+    fi
+
     sudo dpkg -i linux-headers.deb linux-image.deb
-    sudo apt install -f
+    sudo apt install -f -y
 }
 
 # Function to install BBR v3 on CentOS/RHEL/AlmaLinux/Rocky
 install_bbrv3_centos() {
     wget "$CENTOS_HEADERS_URL" -O kernel-headers.rpm
+    if [ $? -ne 0 ]; then
+        echo "下载 kernel-headers.rpm 失败!"
+        exit 1
+    fi
+
     wget "$CENTOS_IMAGE_URL" -O kernel.rpm
+    if [ $? -ne 0 ]; then
+        echo "下载 kernel.rpm 失败!"
+        exit 1
+    fi
+
     sudo rpm -ivh kernel-headers.rpm kernel.rpm
+}
+
+# Function to remove old kernels on Ubuntu/Debian
+remove_old_kernels() {
+    # Get the current running kernel
+    current_kernel=$(uname -r)
+
+    # List all installed kernels except the current one and the 6.4.0 version
+    old_kernels=$(dpkg --list | grep linux-image | awk '{ print $2 }' | grep -v "$current_kernel" | grep -v '6.4.0')
+
+    # If there are old kernels to remove
+    if [ -n "$old_kernels" ]; then
+        echo "正在卸载旧的内核..."
+        sudo apt-get purge -y $old_kernels
+        sudo apt-get autoremove -y
+        sudo update-grub
+        echo "旧内核已删除。"
+    else
+        echo "没有找到其他需要删除的内核。"
+    fi
 }
 
 # Install BBR v3 based on the OS type
 case "$OS" in
     ubuntu|debian)
         install_bbrv3_ubuntu
+        # Remove old kernels and keep only the 6.4.0 kernel
+        remove_old_kernels
         ;;
     centos|rhel|almalinux|rocky)
         install_bbrv3_centos
@@ -56,5 +98,8 @@ sysctl net.ipv4.tcp_congestion_control
 echo "可用的TCP流控算法："
 sysctl net.ipv4.tcp_available_congestion_control
 
-echo "BBR v3 安装和配置完成。"
+# Clean up downloaded files
+echo "清理临时安装包..."
+rm -f linux-headers.deb linux-image.deb kernel-headers.rpm kernel.rpm
 
+echo "BBR v3 安装和配置完成。"
